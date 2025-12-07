c549cb29ae45795b9158462a6c719f5215056044
Bushraturk <bushrahussain068@gmail.com>
2025-12-07 21:17:08 +0500

    First comment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
             <div className={styles.buttons}>
               <Link
                 className="button button--secondary button--lg"
-                to="/physical-ai-textbook/intro">
+                to="/">
                 Start Learning - 13 Week Course
               </Link>
             </div>
